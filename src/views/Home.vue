3c87b27f4aaf8464622d166e795de482fcd16ace
Peter Jang <peter.x.jang@gmail.com>
2020-10-25 11:32:30 -0500

    Create markers from data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,20 +19,26 @@ export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      places: [],
     };
   },
-  created: function () {},
   mounted: function () {
     mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_ACCESS_TOKEN;
     var map = new mapboxgl.Map({
       container: "mymap", // container id
       style: "mapbox://styles/mapbox/streets-v11", // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      center: [131.044, -25.363], // starting position [lng, lat]
+      zoom: 3, // starting zoom
     });
 
-    var popup = new mapboxgl.Popup({ offset: 25 }).setText("Construction on the Washington Monument began in 1848.");
-    var marker = new mapboxgl.Marker().setLngLat([-74.5, 40]).setPopup(popup).addTo(map);
+    this.places = [
+      { lat: -25.363, lng: 131.044, description: "A place in Australia" },
+      { lat: -33.8675, lng: 151.207, description: "The main city!" },
+    ];
+    this.places.forEach((place) => {
+      var popup = new mapboxgl.Popup({ offset: 25 }).setText(place.description);
+      var marker = new mapboxgl.Marker().setLngLat([place.lng, place.lat]).setPopup(popup).addTo(map);
+    });
   },
   methods: {},
 };
