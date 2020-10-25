465ecb595a23081b1cd65a665a5d9715d2372d98
Peter Jang <peter.x.jang@gmail.com>
2020-10-25 10:59:44 -0500

    Add marker with popup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,6 +30,9 @@ export default {
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
     });
+
+    var popup = new mapboxgl.Popup({ offset: 25 }).setText("Construction on the Washington Monument began in 1848.");
+    var marker = new mapboxgl.Marker().setLngLat([-74.5, 40]).setPopup(popup).addTo(map);
   },
   methods: {},
 };
